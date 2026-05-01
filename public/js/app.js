0e45e94421671669fb404df1701b6c5d93f149d6
khairibo32 <khairibo32@example.com>
2026-05-01 20:24:22 +0100

    change same

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useMemo, useRef, useState } from "https://esm.sh/react@18.3.1";
 import { createRoot } from "https://esm.sh/react-dom@18.3.1/client";
 import htm from "https://esm.sh/htm@3.1.1";
+import { inject } from "https://esm.sh/@vercel/analytics";
+
+inject();
+
 
 const html = htm.bind(React.createElement);
 
